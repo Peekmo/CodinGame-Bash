764be76f1f0362a6780f57268452c681da72f689
Peekmo <axel.anceau@gmail.com>
2014-02-28 21:19:34 +0100

    Too tired, can't find a way to do it

--- Conway.sh
+++ Conway.sh
@@ -4,20 +4,24 @@ read r
 read l
 
 line=$r
-for((i=1 ; i<l ; i++)); do
+for((i=1 ; i<$((l)) ; i++)); do
     numbers=(${line// / })
     line=""
     declare -a nums
-    last=${numbers[0]}; nbre=0
+    last=${numbers[0]}; nbre=1
     for i in ${numbers[@]}; do
-        if [[ $i -eq $last ]]; then nbre+=1
-        else
+        if [[ $((i)) -eq ${#numbers[*]} ]]; then
+            nums[${#nums[*]}]=$nbre; 
+            nums[${#nums[*]}]=$last; 
+        elif [[ $i -eq $last ]]; then nbre+=1
+        else 
             nums[${#nums[*]}]=$nbre; 
             nums[${#nums[*]}]=$last; 
             last=$i; 
             nbre=1
         fi
-    done 
+    done
+    echo $line
     line=$(IFS=" "; echo ${nums[@]})
 done
 echo $line
